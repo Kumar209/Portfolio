77fc30075f75c4dd1e2e42e5b8931c2d2fd5568c
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-04-01 17:48:21 +0530

    added new portfolio

--- src/Component/Portfolio/PortfolioAPI.jsx
+++ src/Component/Portfolio/PortfolioAPI.jsx
@@ -1,12 +1,12 @@
 import tindog from '../pic/tindog.png';
 import drumkit from '../pic/drumkit.png';
 import adviceCard from '../pic/adviceCard.png';
+import urlShortener from '../pic/urlShortener.jpg';
 
 const Portfolio_data = [
     {
         id: 1,
         category: "Landing Page",
-        total_likes: 600,
         title: "Tindog",
         image: tindog,
         featured: "Frontend website",
@@ -18,7 +18,6 @@ const Portfolio_data = [
     {
         id: 2,
         category: "Web Application",
-        total_likes: 300,
         title: "Drum kit",
         image: drumkit,
         featured: "Frontend Web App",
@@ -30,13 +29,23 @@ const Portfolio_data = [
     {
         id: 3,
         category: "Web Application",
-        total_likes: 400,
         title: "Advice Card - API based web app",
         image: adviceCard,
         featured: "API based Web Application",
         githublink: "https://github.com/Kumar209/Advice-card",
         live_website_link: "https://kumar209.github.io/Advice-card/",
         desc1: "It is a Web App which generates new adivce on reloading the page or by clicking on dice. It basically uses Advice Slip API for the advice generation.",
+        desc2: "Technology used are HTML, CSS, JavaScript, API",
+    },
+    {
+        id: 4,
+        category: "Web Application",
+        title: "URL Shortener - API based web app",
+        image: urlShortener,
+        featured: "API based Web Application",
+        githublink: "https://github.com/Kumar209/URL-Shortener",
+        live_website_link: "https://kumar209.github.io/URL-Shortener/",
+        desc1: "It is a Web App which converts your very long url into short url for easy use. It basically uses shrtCode API for the short url generation.",
         desc2: "Technology used are HTML, CSS, JavaScript, API",
     },
 ];
